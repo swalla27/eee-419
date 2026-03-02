fcecf35d9dc600bfed4b52350e6ee34d1fb39930
swalla27 <swalla27@asu.edu>
2026-03-02 15:25:38 -0600

    submit extra credit proj

--- projects/extra_credit/extra_credit.py
+++ projects/extra_credit/extra_credit.py
@@ -27,8 +27,9 @@ import os
 
 # Define the number of bits, the number of samples, the frequency of the sinewave, and whether to make a graph of SNR vs BER at the end.
 NUM_BITS = 10_000
-NUM_SAMP = 20
-FREQ = 10e6
+NUM_SAMP = 3
+# FREQ = 10e6
+FREQ = 1 / NUM_SAMP
 PERIOD = 1 / FREQ
 OMEGA = 2*np.pi*FREQ
 MAKE_GRAPH = False
@@ -38,15 +39,21 @@ MAKE_GRAPH = False
 ###########################################
 
 def generate_data(snr_dB: float):
-    """This function generates the data used by both the machine learning algorithm and the classical algorithm.\n
+    """This function generates the data used by both the machine learning algorithm and the classical algorithm.
 
-       *****Inputs*****\n
-       snr_dB: This is the signal to noise ratio expressed in dB. This is used to determine the noise standard deviation.\n
+    Parameters
+    ----------
+    snr_dB : float or int
+        This is the signal to noise ratio expressed in dB. This is used to determine the noise standard deviation.
        
-       *****Outputs*****\n
-       random_bits: This includes all of the randomly generated bits. We use this as the answer key.\n
-       sample_times: The points in time when we sampled the waveform. This is used in the classical demodulation algorithm.\n
-       dirty_array: The sample values of the sinewave we created, with the noise added to it."""
+    Returns
+    -------
+    random_bits : np.array
+       This includes all of the randomly generated bits. We use this as the answer key.
+    sample_times : np.array
+       The points in time when we sampled the waveform. This is used in the classical demodulation algorithm.
+    dirty_array : np.array
+       The sample values of the sinewave we created, with the noise added to it."""
 
     # Find the noise standard deviation associated with this SNR value.
     snr_rat = 10**(snr_dB/10)
@@ -75,16 +82,32 @@ def generate_data(snr_dB: float):
 ############################################
 
 def estimate_phase(sample_times: np.array, sample_values: np.array, freq_guesses: np.array):
-    """This function will estimate the phase of a sampled waveform.\n
+    """This function will estimate the phase of a sampled waveform.
 
-       ****Inputs*****\n
-       sample_times: The points in time when we sampled the waveform. This is used in the classical demodulation algorithm.\n
-       sample_values: The sample values of the sinewave we created, with the noise added to it.\n
-       freq_guesses: The frequency values that we want to test. The final answer will rely on the correct frequency being in this array.\n
+    Parameters
+    ----------
+    sample_times : np.array
+       The points in time when we sampled the waveform. This is used in the classical demodulation algorithm.
+    sample_values : np.array
+       The sample values of the sinewave we created, with the noise added to it.
+    freq_guesses : np.array
+       The frequency values that we want to test. The final answer will rely on the correct frequency being in this array.
 
-       *****Outputs*****\n
-       phase_est: This is the algorithm's estimate for what the phase of that waveform should be."""
+    Returns
+    -------
+    phase_est : float
+       This is the algorithm's estimate for what the phase of that waveform should be."""
     
+    # This code is taken from the module 4 examples and modified slightly. 
+    # We are attempting to find the frequency of the transmitted wave after traversing a noisy channel. 
+    # To do this, we are using the following equation: J = H * inv(HT*H) * HT*X
+    # H is a matrix with the first column being cos terms and the second being sin terms. Each row corresponds to a different sample.
+    # X is the vector of samples, and I chose to name that "sample_values" in my code. 
+    # After guessing a bunch of frequencies, we choose the best one and evaluate the equation for that frequency. 
+    # Once we substitute that frequency in, we arrive at the variable "c", at which point we take the arctangent of c[1] over c[0].
+    # Finally, we use the np.where function to decide whether to return d or its complement with pi. 
+    # The latter is necessary when the point in question lies in the 2nd or 3rd quadrants.
+
     J = list()
     h = np.zeros((NUM_SAMP, 2))
     for freq in freq_guesses:
@@ -110,17 +133,20 @@ def estimate_phase(sample_times: np.array, sample_values: np.array, freq_guesses
     phase_est = np.where(c[0] > 0, d, np.pi-d)
     return phase_est
 
-def evaluate_estimate(phase_est: float, correct_bit: float):
-    """This function will evaluate whether the received phase allows the receiver to correctly reconstruct the original data.\n
-       If the phase is greater than pi/2 or less than -pi/2, then we interpret that to be a bit 1.\n
-       We compare that with the correct bit to make our decision.\n
+def evaluate_estimate(phase_est: float, correct_bit: int):
+    """Now that RX has estimated the phase, we need to see whether that's what TX actually said.
+    If the phase is greater than pi/2 or less than -pi/2, then we interpret that to be a bit 1.
     
-       *****Inputs*****\n
-       phase_est: The phase estimate for this waveform at the receiver.\n
-       correct_bit: The actual bit used to create this waveform at the transmitter.\n
+    Parameters
+    ----------
+    phase_est : float
+        The phase estimate at RX.
+    correct_bit : int
+        The bit that TX wanted to communicate.
 
-       *****Outputs*****\n
-       bool: The function will return True if the received bit matches the transmitted bit, and False otherwise."""
+    Returns
+    -------
+    The function will return True if the received bit matches the transmitted bit, and False otherwise."""
 
     # If the phase estimate is closer to a phase of -pi, then interpret this bit as a 1. Otherwise 0.
     if (phase_est > np.pi/2) or (phase_est < -np.pi/2):
@@ -138,16 +164,22 @@ def evaluate_estimate(phase_est: float, correct_bit: float):
 ##### Estimate the phase using classical demodulation #####
 ###########################################################
 
-def demod_cl(random_bits: np.array, sample_times: np.array, dirty_array: np.array):
-    """This function demodulates the received signals using the classical methods.\n
+def demod_classic(random_bits: np.array, sample_times: np.array, dirty_array: np.array):
+    """This function demodulates the received signals using the classical method.
     
-       *****Inputs*****\n
-       random_bits: This includes all of the randomly generated bits. We use this as the answer key.\n
-       sample_times: The points in time when we sampled the waveform. This is used in the classical demodulation algorithm.\n
-       dirty_array: The sample values of the sinewave we created, with the noise added to it.\n
+    Parameters
+    ----------
+    random_bits : np.array
+        This includes all of the randomly generated bits. We use this as the answer key.
+    sample_times : np.array
+        The points in time when we sampled the waveform. This is used in the classical demodulation algorithm.
+    dirty_array : np.array
+        The sample values of the sinewave we created, with the noise added to it.
 
-       *****Outputs*****\n
-       ber: The bit error rate using the classical demodulation technique."""
+    Returns
+    -------
+    ber : float
+        The bit error rate using the classical demodulation technique."""
 
     # Initialize the array containing which frequencies we are going to guess and a variable storing the number of bit errors.
     freq_guesses = np.linspace(FREQ/10, FREQ*10, 20)
@@ -156,9 +188,10 @@ def demod_cl(random_bits: np.array, sample_times: np.array, dirty_array: np.arra
     # Loop over every bit in the generated data and determine whether that bit was interpreted correctly at the receiver.
     for idx, sample_values in enumerate(dirty_array):
         phase_est = estimate_phase(sample_times, sample_values, freq_guesses)
+        estimate_is_correct = evaluate_estimate(phase_est, random_bits[idx])
 
         # Add one to the count of bit errors if the noise caused the receiver to interpet the bit wrong.
-        if not evaluate_estimate(phase_est, random_bits[idx]):
+        if not estimate_is_correct:
             bit_errors += 1
 
     # Calculate the bit error rate and return that value to the next level.
@@ -169,16 +202,22 @@ def demod_cl(random_bits: np.array, sample_times: np.array, dirty_array: np.arra
 ##### Estimate the phase using machine learning #####
 #####################################################
 
-def demod_ml(random_bits: np.array, dirty_array: np.array, num_neigh=100):
+def demod_knn(random_bits: np.array, dirty_array: np.array, num_neigh=100):
     """This function demodulates the signal using machine learning, more specifically the KNN algorithm.\n
        
-       *****Inputs*****
-       random_bits: This includes all of the randomly generated bits. We use this as the answer key.\n
-       dirty_array: The sample values of the sinewave we created, with the noise added to it.\n
-       num_neigh: The number of neighbors used for the KNN algorithm. Default value is set to 100.\n
+    Parameters
+    ----------
+    random_bits : np.array
+        This includes all of the randomly generated bits. We use this as the answer key.\n
+    dirty_array : np.array
+        The sample values of the sinewave we created, with the noise added to it.\n
+    num_neigh : int
+        The number of neighbors used for the KNN algorithm. Default value is set to 100.\n
 
-       *****Outputs*****
-       ber: The bit error rate for the machine learning algorithm."""
+    Returns
+    -------
+    ber: float
+        The bit error rate for the machine learning algorithm."""
 
     # Create the test and training split.
     df = pd.DataFrame(data=dirty_array)
@@ -208,12 +247,12 @@ if __name__ == "__main__":
 
     # Generate the data and demodulate using both the machine learning and classical algorithms.
     # Next, print that information to the terminal as described in the prompt.
-    random_bits, sample_times, dirty_array = generate_data(snr_dB=-10)
+    random_bits, sample_times, dirty_array = generate_data(snr_dB=0)
 
     # cl stands for the classical algorithm.
     # ml stands for the machine learning algorithm, KNN in this case.  
-    ber_cl = demod_cl(random_bits, sample_times, dirty_array)
-    ber_ml = demod_ml(random_bits, dirty_array)
+    ber_cl = demod_classic(random_bits, sample_times, dirty_array)
+    ber_ml = demod_knn(random_bits, dirty_array)
     print([round(ber_cl, 3)])
     print([round(ber_ml, 3)])
     
@@ -223,7 +262,7 @@ if __name__ == "__main__":
         sys.exit()
 
     # Create arrays of the SNR values and bit error rates for each demodulation technique.
-    snr_values = np.arange(-20, 10, step=1)
+    snr_values = np.arange(-20, 10+1, step=1)
     ber_vals_ml = np.zeros(snr_values.size)
     ber_vals_cl = np.zeros(snr_values.size)
 
@@ -237,8 +276,8 @@ if __name__ == "__main__":
         random_bits, sample_times, dirty_array = generate_data(snr_dB)
 
         # Demodulate the waveform using both the machine learning algorithm and the classical demodulation technique.
-        ber_cl = demod_cl(random_bits, sample_times, dirty_array)
-        ber_ml = demod_ml(random_bits, dirty_array)
+        ber_cl = demod_classic(random_bits, sample_times, dirty_array)
+        ber_ml = demod_knn(random_bits, dirty_array)
 
         # Add those results to arrays for each algorithm.
         ber_vals_cl[idx] = ber_cl
@@ -249,13 +288,13 @@ if __name__ == "__main__":
         print(f'snr = {snr_dB}; elapsed = {t1-t0:.3f} s')
 
     # Create a graph comparing the bit error rates for the machine learning and classical techniques.
-    plt.scatter(snr_values, ber_vals_cl, label='Classical Method')
-    plt.scatter(snr_values, ber_vals_ml, label='Machine Learning')
-    plt.xlabel('SNR Values (dB)')
-    plt.ylabel('Bit Error Rates (Ratio)')
-    plt.title('SNR vs BER for Various Algorithms')
+    plt.scatter(snr_values, ber_vals_cl, label='Classical Method', color='tab:blue')
+    plt.scatter(snr_values, ber_vals_ml, label='Machine Learning', color='tab:red')
+    plt.xlabel('Signal to Noise Ratio (dB)')
+    plt.ylabel('Bit Error Rate (Unitless)')
+    plt.title('SNR vs BER')
     plt.legend()
     plt.grid(True)
-    plt.savefig('projects/extracredit/ber_vs_snr.png', dpi=300)
+    plt.savefig('projects/extra_credit/ber_vs_snr.png', dpi=300)
     plt.show()
 
