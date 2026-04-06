9b7e558412b3f880ff0bb4947ca1f80a67639c62
swalla27 <swalla27@asu.edu>
2026-04-06 15:50:00 -0500

    class and static methods

--- notes/M13/scratchpad.py
+++ notes/M13/scratchpad.py
@@ -1,4 +1,8 @@
+import datetime
+
 class Employee:
+
+    # This is a class variable, do not change this for a single instance. Always do it for the entire class.
     raise_amt = 1.04
 
     def __init__(self, first, last, pay):
@@ -14,9 +18,11 @@ class Employee:
         self.pay = int(self.pay * self.raise_amt)
 
     def __repr__(self):
+        # A dunder method used to make a string representation of your class, programmers are the intended audience.
         return f'Employee({self.first}, {self.last}, {self.pay})'
     
     def __str__(self):
+        # A dunder method used to make a string representation of your class, end user is the intended audience.
         return f'{self.fullname()} - {self.email}'
     
     def __add__(self, other):
@@ -24,6 +30,27 @@ class Employee:
     
     def __len__(self):
         return len(self.fullname())
+    
+    @classmethod
+    # Do not run this from an instance, always do it from the class.
+    # It accesses the class (cls) instead of the instance (self).
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    # An alternative contructor, meaning you can create an instance this way too.
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+    
+    @staticmethod
+    # A static method does not pass the instance (self) or the class (cls) as input.
+    # If you don't access self or cls in the function, then it should probably be a static method.
+    def is_workday(day):
+        if (day.weekday() == 5) or (day.weekday() == 6):
+            return False
+        else:
+            return True
 
 class Developer(Employee):
     raise_amt = 1.10
@@ -56,9 +83,21 @@ class Manager(Employee):
         for emp in self.employees:
             print('-->', emp.fullname())
 
-dev_1 = Developer('Corey', 'Shafer', 50_000, 'Python')
-dev_2 = Developer('Test', 'Employee', 60_000, 'Java')
+emp_1 = Employee('Corey', 'Shafer', 50_000)
+emp_2 = Employee('Test', 'Employee', 60_000)
 
-print(repr(dev_1))
-print(str(dev_2))
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
 
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.fullname())
+
+print(Employee.is_workday(datetime.date(2016, 7, 11)))
+
+
+# print(Employee.raise_amt)
+
+# Employee.set_raise_amt(1.20)
+
+# print(emp_1.raise_amt)
+# print(emp_2.raise_amt)
