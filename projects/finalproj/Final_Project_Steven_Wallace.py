bc0163637a61bef824fc3c3651e3c4827e68d8ae
swalla27 <swalla27@asu.edu>
2026-04-24 11:42:21 -0500

    added grayscale version

--- projects/finalproj/Final_Project_Steven_Wallace.py
+++ projects/finalproj/Final_Project_Steven_Wallace.py
@@ -24,10 +24,8 @@ import timm
 
 import torchvision
 from torchvision import transforms
-from tqdm.notebook import tqdm
 
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
 import time
 import sys
@@ -39,18 +37,18 @@ import os
 
 BATCH_SIZE = 128
 NUM_CLASSES = 10
-EPOCHS = 25
+EPOCHS = 5
 INCLUDE_LIST = ['ship', 'truck']
+SHOW_GRAPHS = True
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-###################
-##### Classes #####
-###################
+#################################
+##### Functions and Classes #####
+#################################
 
 class CIFAR10_NET(nn.Module):
     """
-    Define the neural network.
+    Define the neural network used for predictions on the CIFAR10 dataset.
     """
 
     def __init__(self, num_classes):
@@ -82,130 +80,161 @@ class CIFAR10_NET(nn.Module):
         # x = self.drop2(x)
         # x = self.fc2(x)
         return x
+    
 
-###########################
-##### Gather the data #####
-###########################
+def custom_accuracy(testloader, model):
+    """
+    A custom accuracy function using code from the EEE419 PyTorch examples.
+    """
+
+    num_correct = 0
+    num_attempts = 0
+
+    for images, labels in testloader:
+
+        with torch.no_grad():
+
+            outputs = model(images)
+            guesses = torch.argmax(outputs, 1)
+
+            num_guess = len(guesses)
+            num_right = torch.sum(labels == guesses).item()
+
+            num_correct  += num_right
+            num_attempts += num_guess
+
+    accuracy = 100*num_correct/num_attempts
+    return accuracy
+
+def training_loop(transform):
+    """
+    Define a training loop which will be used for the RGB and Grayscale situations.
+    """
+
+    # Load the training data and apply the requested transform.
+    trainset = torchvision.datasets.CIFAR10(root='~/CIFAR10_data', train=True, 
+                                download=True, transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
+                                            shuffle=True)
+
+    # Load the testing data and apply the requested transform.
+    testset = torchvision.datasets.CIFAR10(root='~/CIFAR10_data', train=False, 
+                                download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
+                                            shuffle=False)
+
+    # Create an instance of the CIFAR10_NET class and send it to the GPU if available.
+    model = CIFAR10_NET(NUM_CLASSES)
+    model.to(DEVICE)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adadelta(model.parameters())
+
+    # Initialize lists to store the training and testing losses, so they can be graphed later.
+    train_losses = list()
+    test_losses = list()
+
+    # Record the time when training began.
+    t_start = time.time()
+
+    # Loop over the entire dataset "EPOCHS" times.
+    for epoch in range(EPOCHS):
+
+        # Train the model for this epoch.
+        model.train()
+        running_loss = 0.0
+
+        # Begin to loop over the training loader.
+        for batch_idx, (images, labels) in enumerate(trainloader):
+
+            # Send the images and labels to the GPU if available.
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+
+            # Carry out forward and back propagation on the neural network.
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item() * labels.size(0)
+
+        # Keep record of the training loss for each epoch.
+        train_loss = running_loss / len(trainloader.dataset)
+        train_losses.append(train_loss)
+
+        # Evaluate the model for this epoch.
+        model.eval()
+        running_loss = 0.0
+
+        # Begin to loop over the testing loader, making sure to leave the weights and biases untouched.
+        with torch.no_grad():
+            for images, labels in testloader:
+
+                # Move the inputs and images to the GPU if available.
+                images, labels = images.to(DEVICE), labels.to(DEVICE)
+
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item() * labels.size(0)
+
+        # Keep track of the testing loss for each epoch so that it can be graphed later.
+        test_loss = running_loss / len(trainloader.dataset)
+        test_losses.append(test_loss)
+
+        print(f'Epoch {epoch}/{EPOCHS}:\n\tTraining Loss = {train_loss}\n\tTesting Loss = {test_loss}')
+
+    # Record the time when training ended.
+    t_end = time.time()
+
+    # Plot the training and test losses vs epoch number. This gives me information about overfitting or underfitting.
+    if SHOW_GRAPHS:
+        plt.plot(train_losses, label='Training Loss')
+        plt.plot(test_losses, label='Testing Losses')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Epoch vs Loss')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+    # Return the model accuracy and training time.
+    accuracy = custom_accuracy(testloader, model)
+    duration = t_start - t_end
+    return accuracy, duration
+
+####################
+##### RGB Case #####
+####################
 
 # Create the data transform based on tensor conversion and normalization.
 to_tensor = transforms.ToTensor()
 normalize = transforms.Normalize([0.5], [0.5])
 transform = transforms.Compose([to_tensor, normalize])
 
-# Load the training data.
-trainset = torchvision.datasets.CIFAR10(root='~/CIFAR10_data', train=True, 
-                            download=True, transform=transform)
+# Run the training loop with this transform, which leaves the RGB channels intact.
+rgb_acc, rgb_time = training_loop(transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
-                                          shuffle=True)
+##########################
+##### Grayscale Case #####
+##########################
 
-# Load the testing data.
-testset = torchvision.datasets.CIFAR10(root='~/CIFAR10_data', train=False, 
-                            download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
-                                          shuffle=False)
+# The grayscale code differs only in the transformation. I have added a grayscale transform before normalization.
+to_tensor = transforms.ToTensor()
+grayscale = transforms.Grayscale(num_output_channels=1)
+normalize = transforms.Normalize([0.5], [0.5])
+transform = transforms.Compose([to_tensor, grayscale, normalize])
 
-#####################################
-##### Create the neural network #####
-#####################################
+# Run the training loop with this new transform.
+gray_acc, gray_time = training_loop(transform)
 
-# Create an instance of the CIFAR10_NET class and send it to the GPU if available.
-model = CIFAR10_NET(NUM_CLASSES)
-model.to(device)
+#######################################################
+##### Display the results in the requested format #####
+#######################################################
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adadelta(model.parameters())
-
-train_losses = list()
-test_losses = list()
-
-#########################
-##### Training Loop #####
-#########################
-num_batches = len(trainloader)
-
-# Loop over the entire dataset "EPOCHS" times.
-for epoch in range(EPOCHS):
-
-    # Train the model for this epoch.
-    model.train()
-    running_loss = 0.0
-
-    # Begin to loop over the training loader.
-    for batch_idx, (images, labels) in enumerate(trainloader):
-
-        # Send the images and labels to the GPU if available.
-        images, labels = images.to(device), labels.to(device)
-
-        # Carry out forward and back propagation on the neural network.
-        optimizer.zero_grad()
-        output = model(images)
-        loss = criterion(output, labels)
-        loss.backward()
-        optimizer.step()
-
-        # Print loss information to the terminal for every 100th batch.
-        if batch_idx % 100 == 0:
-            batch_loss = loss.mean().item()
-            print("Epoch {}/{}\tBatch {}/{}\tLoss: {}" \
-                  .format(epoch, EPOCHS, batch_idx, num_batches, batch_loss))
-            
-        running_loss += loss.item() * labels.size(0)
-
-    # Keep record of the training loss for each epoch.
-    train_loss = running_loss / len(trainloader.dataset)
-    train_losses.append(train_loss)
-
-    # Evaluate the model for this epoch.
-    model.eval()
-    running_loss = 0.0
-
-    # Begin to loop over the testing loader, making sure to leave the weights and biases untouched.
-    with torch.no_grad():
-        for images, labels in testloader:
-
-            # Move the inputs and images to the GPU if available.
-            images, labels = images.to(device), labels.to(device)
-
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            running_loss += loss.item() * labels.size(0)
-
-    # Keep track of the testing loss for each epoch so that it can be graphed later.
-    test_loss = running_loss / len(trainloader.dataset)
-    test_losses.append(test_loss)
-
-# Plot the training and test losses vs epoch number. This gives me information about overfitting or underfitting.
-plt.plot(train_losses, label='Training Loss')
-plt.plot(test_losses, label='Testing Losses')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Epoch vs Loss')
-plt.grid(True)
-plt.legend()
-plt.show()
-   
-########################
-##### Testing Loop #####
-########################
-
-# num_correct = 0
-# num_attempts = 0
-
-# for images, labels in testloader:
-    
-#     # Send the images and labels to the GPU if available.
-#     images, labels = images.to(device), labels.to(device)
-
-#     with torch.no_grad():
-
-#         outputs = model(images)
-#         guesses = torch.argmax(outputs, 1)
-
-#         num_guess = len(guesses)
-#         num_right = torch.sum(labels == guesses).item()
-
-#         num_correct += num_right
-#         num_attempts += num_guess
-
-# print("Total test accuracy:", 100*num_correct/num_attempts,"%")
+print(f'RGB Accuracy: {rgb_acc:.1f}%')
+print(f'Grayscale Accuracy: {gray_acc:.1f}%')
+print(f'RGB Runtime: {rgb_time:.5f} seconds')
+print(f'Grayscale Runtime: {gray_time:.5f} seconds')
+print('Steven Wallace recommends RGB algorithm')
